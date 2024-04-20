d558d41a3ded2b5902d6e687326977ddb15227b4
Almir Rogério Camolesi <camolesi@fema.edu.br>
2024-04-19 21:02:49 -0300

    Add project files.

---
+++ Contexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WCFDados
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+
+        public Contexto(): base ("TAT12024") { }
+
+    }
+}
